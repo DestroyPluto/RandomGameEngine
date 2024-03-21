824a6ebf40a15489a3a70e769770980733fe3d96
rino776 <rinoharrold@gmail.com>
2024-03-21 23:25:23 +1000

    added warning about memory management

--- Include/Math/PointArray.h
+++ Include/Math/PointArray.h
@@ -9,7 +9,9 @@ public:
     PointArray(Point* point, size_t size);
     PointArray();
     //copies the points into the passed in array.
+    //Note: arr memory is owned by the caller.
     void toFloatArray(float* arr, size_t& size);
+    void add(Point point){m_pointArray.push_back(point);};
 
 private:
     std::vector<Point> m_pointArray;
