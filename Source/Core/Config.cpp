e810088f7df54070d8d2bcc421c0f63ef63978a1
DestroyPluto <rinoharrold@gmail.com>
2025-09-14 00:07:14 +1000

    load config from file

--- Source/Core/Config.cpp
+++ Source/Core/Config.cpp
@@ -1,6 +1,7 @@
 #include "Config.h"
 #include <sstream>
 #include <fstream>
+#include <filesystem>
 #include "HgLogger.h"
 
 using namespace core;
@@ -36,8 +37,33 @@ void Config::setOption(std::string key, std::string option){
 }
 
 void Config::loadFromFile(std::string path){
-    //do nothing for now lol
+        
+    if (!std::filesystem::exists(path)) {
+        HgLogger::logWarning("Config file %s does not exist.", path.c_str());
+        return;
+    }
 
+    std::lock_guard<std::mutex> lock(m_configMutex); // Ensure thread safety
+    std::ifstream file(path);
+       if (!file.is_open()) {
+        HgLogger::logError("%s could not be opened for reading.", path.c_str());
+        return;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        // Ignore empty lines and comments
+        if (line.empty() || line[0] == '#') continue;
+
+        size_t delimPos = line.find('=');
+        if (delimPos == std::string::npos) continue; // Invalid line
+
+        std::string key = line.substr(0, delimPos);
+        std::string value = line.substr(delimPos + 1);
+        m_configValues[key] = value;
+    }
+    file.close();
+    HgLogger::logDebug("Config loaded from %s", path.c_str());
 }
 
 void Config::saveToFile(std::string path){
--- Source/main.cpp
+++ Source/main.cpp
@@ -62,6 +62,7 @@ class Game{
             auto lastTime = std::chrono::high_resolution_clock::now();
             
             SceneManager::getInstance()->setEngine((RenderingPlugin*) m_engine.get());
+            m_config->loadFromFile("config.cfg");
             GameManager game = GameManager(m_config);
 
             //ensure engine is initialized before we start the game
